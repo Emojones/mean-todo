9280fb303297f6769d9e36f0d994e36fe84a68da
Michael <emonomics@gmail.com>
2016-10-02 14:37:37 -0400

    moved router to api/index.js

---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/todos', function(req,res){
+  res.json({todos:[]});
+})
+
+
+//TODO: add POST route to create new entries
+//TODO: add PUT route to update existing entries
+//TODOL add DELETE route to delete entries
+
+module.exports = router;
--- src/app.js
+++ src/app.js
@@ -1,21 +1,11 @@
 'use strict';
 
 var express = require('express');
+var router = require('./api');
 
 var app = express();
 
 app.use('/', express.static('public'));
-
-var router = express.Router();
-
-router.get('/todos', function(req,res){
-  res.json({todos:[]});
-})
-
-
-//TODO: add POST route to create new entries
-//TODO: add PUT route to update existing entries
-//TODOL add DELETE route to delete entries
 
 app.use('/api', router);
 
